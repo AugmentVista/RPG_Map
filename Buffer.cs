d9d684526a6f85ba05a7e3a933fa40bee092a065
AugmentVista <AuAgments@gmail.com>
2023-11-29 22:16:18 -0400

    FUNNY UNICODE RESEARCH

--- Buffer.cs
+++ Buffer.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Numerics;
-using System.ComponentModel.Design;
-using System.Reflection.Metadata.Ecma335;
+
 
 namespace RPG_Map
 {
@@ -28,7 +27,7 @@ namespace RPG_Map
 
         public static void DisplayBuffer(int scale)
         {
-            Console.Write(" "); // add a space before the first row
+            
             for (int Y = 0; Y < firstBuffer.GetLength(0); Y++)
             {
                 for (int columnScale = 0; columnScale < scale; columnScale++)
@@ -46,265 +45,57 @@ namespace RPG_Map
                             switch (MapElements)
                             {
                                 case '`':
-                                    Console.ForegroundColor = ConsoleColor.Gray;
+                                    Console.ForegroundColor = ConsoleColor.Magenta;
                                     break;
                                 case '^':
                                     Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.BackgroundColor = ConsoleColor.Black;
                                     break;
                                 case '*':
                                     Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                    Console.BackgroundColor = ConsoleColor.Black;
                                     break;
                                 case '~':
                                     Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.BackgroundColor = ConsoleColor.Black;
                                     break;
                                 case '░':
                                     Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.BackgroundColor = ConsoleColor.Black;
                                     break;
                                 case '█':
                                     Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                    Console.BackgroundColor = ConsoleColor.Black;
                                     break;
-                                case '@':
+                                case '☻':
                                     Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.BackgroundColor = ConsoleColor.Black;
+                                    break;
+                                case '☺':
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.BackgroundColor = ConsoleColor.Yellow;
                                     break;
                             }
                             Console.SetCursorPosition(X * scale + columnScale, Y * scale + rowScale);
                             Console.Write(MapElements);
                         }
                     }
-                    Console.WriteLine();
-                    Console.Write(" "); // adds a space before the start of each row after the first
                 }
             }
             Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.Black;    
+            Console.WriteLine(" ");
             Console.WriteLine("     Map legend:  ");
             Console.WriteLine("     ^ = mountain ");
             Console.WriteLine("     * = Tree     ");
             Console.WriteLine("     ` = grass    ");
             Console.WriteLine("     ~ = water    ");
+            Console.WriteLine("⬠");
 
             Array.Copy(firstBuffer, secondBuffer, MapData.map.Length);
         }
     }
-
-    public class MapData
-    {
-        static public char character;
-        static int playerRow = 1; // Initial player position
-        static int playerCol = 1;
-        public static int scale;
-        static char playerCharacter = '@';
-        public static char[,] map;
-
-        static string[] border = new string[]
-        {
-          "╔","╗","╝","╚", "║","═"
-        };
-
-        static void Main(string[] args)
-        {
-            TxtFileToMapArray();
-
-            ConsoleKeyInfo keyInfo;
-            do
-            {
-                keyInfo = Console.ReadKey(true);
-                Console.SetCursorPosition(0, 0);
-                HandleKeyPress(keyInfo.Key);
-                DrawMap();
-                DrawPlayer();
-                Buffer.DisplayBuffer(1);
-                DrawBorder(1);
-            } while (keyInfo.Key != ConsoleKey.Escape);
-        }
-
-        public static void TxtFileToMapArray()
-        {
-            string[] lines = File.ReadAllLines("Map.txt");
-
-            map = new char[lines.GetLength(0), lines[0].Length];
-            Buffer.firstBuffer = new char[lines.GetLength(0), lines[0].Length];
-            Buffer.secondBuffer = new char[lines.GetLength(0), lines[0].Length];
-            for (int i = 0; i < lines.GetLength(0); i++)
-            {
-                for (int j = 0; j < lines[0].Length; j++)
-                {
-                    map[i, j] = lines[i][j];
-                }
-            }
-        }
-
-        public static void DrawMap()
-        {
-            Array.Copy(map, Buffer.secondBuffer, map.Length);
-        }
-        public static void DrawPlayer()
-        {
-            Buffer.secondBuffer[playerCol, playerRow] = playerCharacter;
-        }
-
-        static void HandleKeyPress(ConsoleKey key)
-        {
-            switch (key)
-            {
-                case ConsoleKey.UpArrow:
-                    MovePlayer(0, -1);
-                break;
-
-                case ConsoleKey.DownArrow:
-                    MovePlayer(0, 1);
-                break;
-
-                case ConsoleKey.LeftArrow:
-                    MovePlayer(-1, 0);
-                break;
-
-                case ConsoleKey.RightArrow:
-                    MovePlayer(1, 0);
-                break;
-
-                case ConsoleKey.W:
-                    MovePlayer(0, -1);
-                break;
-
-                case ConsoleKey.S:
-                    MovePlayer(0, 1);
-                break;
-
-                case ConsoleKey.A:
-                    MovePlayer(-1, 0);
-                break;
-
-                case ConsoleKey.D:
-                    MovePlayer(1, 0);
-                break;
-            }
-        }
-
-        static void MovePlayer(int rowChange, int columnChange)
-        {
-            int newRow = playerRow + rowChange;
-            int newCol = playerCol + columnChange;
-
-            if (IsValidMove(newRow, newCol))
-            {
-                // Update the player's position
-                playerRow = newRow;
-                playerCol = newCol;
-            }
-        }
-
-
-        static bool IsValidMove(int newRow, int newCol)
-        {
-            // Check if the new position is within the bounds of the map
-            if (newRow >= 0 && newRow < map.GetLength(0) + 1 && newCol >= 0 && newCol < map.GetLength(1) + 1)
-            {
-                switch (map[newCol, newRow])
-                {
-                    case '`':
-                    case '^':
-                    case '~':
-                    case '░':
-                        return true;
-                    case '*':
-                    case '█':
-                        return false;
-                }
-
-            }
-            return false;
-
-            //    return true;
-
-            //switch (map[newCol, newRow])
-            //{
-            //    case '`':
-            //    case '^':
-            //    case '~':
-            //    case '░':
-            //        return true;
-            //    case '*':
-            //    case '█':
-            //        return false;
-            //}
-
-            //return false;
-        }
-
-        static void DrawBorder(int scale)
-        {
-            int mapWidth = map.GetLength(1);
-            Console.WriteLine(map.GetLength(1) + "Cat");
-            int mapHeight = map.GetLength(0);
-            Console.WriteLine(map.GetLength(0) + "Cat");
-            Console.WriteLine(scale + "Cat");
-            int HorizontalWall = 1;
-            int VerticalWall = 1;
-            int totalWidth = mapWidth * scale;
-            int totalHeight = mapHeight * scale;
-
-            foreach (string ASCll in border)
-            {
-                switch (ASCll)
-                {
-                    case "╔":
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        break;
-                    case "╗":
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        break;
-                    case "╝":
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        break;
-                    case "╚":
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        break;
-                    case "║":
-                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-                        break;
-                    case "═":
-                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-                        break;
-                }
-            }
-
-            Console.SetCursorPosition(0, 0);
-            Console.Write(border[0]);
-            Console.SetCursorPosition(totalWidth, 0);
-            Console.Write(border[1]);
-            if (HorizontalWall >= totalWidth)
-            {
-                HorizontalWall = totalWidth;
-            }
-            if (VerticalWall >= totalHeight) 
-            { 
-                VerticalWall = totalHeight; 
-            }
-            for (int i = 0; i < totalWidth; i++)
-            {
-                Console.SetCursorPosition(HorizontalWall, 0); // ceiling
-                Console.Write(border[5]);
-                Console.SetCursorPosition(HorizontalWall, totalHeight); // floor
-                Console.Write(border[5]);
-                HorizontalWall++;
-            }
-            for (int j = 0; j < totalHeight; j++)
-            {
-                Console.SetCursorPosition(0, VerticalWall); // lefthand wall 
-                Console.Write(border[4]);
-                Console.SetCursorPosition(totalWidth, VerticalWall); // righthand wall 
-                Console.Write(border[4]);
-                VerticalWall++;
-            }
-            Console.WriteLine(); // this is needed to prevent the map from overlapping with the border
-            Console.SetCursorPosition(totalWidth, 0);
-            Console.Write(border[1]);
-            Console.SetCursorPosition(0, totalHeight);
-            Console.Write(border[3]);
-            Console.SetCursorPosition(totalWidth, totalHeight);
-            Console.Write(border[2]);
-        }  
-    }
+}
 
     //public class TextRPG
     //{
@@ -532,5 +323,3 @@ namespace RPG_Map
     //    }
 
 
-    //}
-}
---
+++ MapData.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Map
+{
+    public class MapData
+    {
+        static public char character;
+        static int playerRow = 1; // Initial player position
+        static int playerCol = 1;
+        public static int scale;
+        static char playerCharacter = '☻';
+        static char enemyCharacter = '☺';
+        public static char[,] map;
+
+        static string[] border = new string[]
+        {
+          "╔","╗","╝","╚", "║","═"
+        };
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            TxtFileToMapArray();
+
+
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                Console.SetCursorPosition(0, 0);
+                HandleKeyPress(keyInfo.Key);
+                DrawMap();
+                DrawPlayer();
+                Buffer.DisplayBuffer(1);
+                DrawBorder(1);
+            } while (keyInfo.Key != ConsoleKey.Escape);
+        }
+
+        public static void TxtFileToMapArray()
+        {
+            string[] lines = File.ReadAllLines("Map.txt");
+
+            map = new char[lines.GetLength(0), lines[0].Length];
+            Buffer.firstBuffer = new char[lines.GetLength(0), lines[0].Length];
+            Buffer.secondBuffer = new char[lines.GetLength(0), lines[0].Length];
+            for (int i = 0; i < lines.GetLength(0); i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    map[i, j] = lines[i][j];
+                }
+            }
+        }
+
+        public static void DrawMap()
+        {
+            Array.Copy(map, Buffer.secondBuffer, map.Length);
+        }
+        public static void DrawPlayer()
+        {
+            Buffer.secondBuffer[playerCol, playerRow] = playerCharacter;
+        }
+
+        static void HandleKeyPress(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    MovePlayer(0, -1);
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    MovePlayer(0, 1);
+                    break;
+
+                case ConsoleKey.LeftArrow:
+                    MovePlayer(-1, 0);
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    MovePlayer(1, 0);
+                    break;
+
+                case ConsoleKey.W:
+                    MovePlayer(0, -1);
+                    break;
+
+                case ConsoleKey.S:
+                    MovePlayer(0, 1);
+                    break;
+
+                case ConsoleKey.A:
+                    MovePlayer(-1, 0);
+                    break;
+
+                case ConsoleKey.D:
+                    MovePlayer(1, 0);
+                    break;
+            }
+        }
+
+        static void MovePlayer(int rowChange, int columnChange)
+        {
+            Console.CursorVisible = false;
+            int newRow = playerRow + rowChange;
+            int newCol = playerCol + columnChange;
+
+            if (IsValidMove(newRow, newCol))
+            {
+                // Update the player's position
+                playerRow = newRow;
+                playerCol = newCol;
+            }
+        }
+
+
+        static bool IsValidMove(int newRow, int newCol)
+        {
+            if (newRow > 0 && newRow < map.GetLength(1) && newCol > 0 && newCol < map.GetLength(0))
+            {
+                switch (map[newCol, newRow])
+                {
+                    case '`':
+                    case '^':
+                    case '~':
+                    case '░':
+                    case '†':
+                        return true;
+                    case '*':
+                    case '█':
+                        return false;
+                }
+
+            }
+            return false;
+        }
+
+        static void DrawBorder(int scale)
+        {
+            int mapWidth = map.GetLength(1);
+            int mapHeight = map.GetLength(0);
+            int HorizontalWall = 1;
+            int VerticalWall = 1;
+            int totalWidth = mapWidth * scale;
+            int totalHeight = mapHeight * scale;
+
+            foreach (string ASCll in border)
+            {
+                switch (ASCll)
+                {
+                    case "╔":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case "╗":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case "╝":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case "╚":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case "║":
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        break;
+                    case "═":
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        break;
+                }
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write(border[0]);
+            Console.SetCursorPosition(totalWidth, 0);
+            Console.Write(border[1]);
+            if (HorizontalWall >= totalWidth)
+            {
+                HorizontalWall = totalWidth;
+            }
+            if (VerticalWall >= totalHeight)
+            {
+                VerticalWall = totalHeight;
+            }
+            for (int i = 0; i < totalWidth; i++)
+            {
+                Console.SetCursorPosition(HorizontalWall, 0); // ceiling
+                Console.Write(border[5]);
+                Console.SetCursorPosition(HorizontalWall, totalHeight); // floor
+                Console.Write(border[5]);
+                HorizontalWall++;
+            }
+            for (int j = 0; j < totalHeight; j++)
+            {
+                Console.SetCursorPosition(0, VerticalWall); // lefthand wall 
+                Console.Write(border[4]);
+                Console.SetCursorPosition(totalWidth, VerticalWall); // righthand wall 
+                Console.Write(border[4]);
+                VerticalWall++;
+            }
+            Console.SetCursorPosition(totalWidth, 0);
+            Console.Write(border[1]);
+            Console.SetCursorPosition(0, totalHeight);
+            Console.Write(border[3]);
+            Console.SetCursorPosition(totalWidth, totalHeight);
+            Console.Write(border[2]);
+
+        }
+    }
+}
